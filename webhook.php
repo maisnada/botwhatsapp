44cf5cf719f735ce4388621beb8aaad491a50e11
Marcelo Moreno <marcelo.moreno@univali.br>
2024-12-30 09:31:52 -0300

    log

--- webhook.php
+++ webhook.php
@@ -6,4 +6,7 @@ $token = $_GET["hub_verify_token"];
 
 echo $challenge;
 
-//print_r($_GET);
+$myfile = fopen("text.txt", "a") or die("Unable to open file!");
+fwrite($myfile, print_r($_GET, true));
+fwrite($myfile, print_r($_POST, true));
+fclose($myfile);
